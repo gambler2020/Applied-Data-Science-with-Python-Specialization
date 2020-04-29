aba60037e89448391efd3a2563c1a86dac8876c2
Mohammad Aziz <aziznitham@gmail.com>
2020-04-29 13:51:19 +0530

    Add files via upload

---
+++ Python/dir12.py
@@ -0,0 +1,5 @@
+sales_record={"price": 3.24, "num_items": 4, "person": "Mohammad"}
+sales_statement="{} bought {} item(s) at a price of {} each for a total of {}"
+print(sales_statement.format(sales_record["person"], sales_record["num_items"],
+    sales_record["price"], sales_record["num_items"]*sales_record["price"]))
+
